7a4795a60ef0caa5f7d803b4f4fa1a07307ddbd4
Fábio Souza <37919743+Fgrauersouza@users.noreply.github.com>
2024-07-31 22:44:01 -0300

    Alterações - Instance
    Criação da instance e bloco de código INCOMPLETO, adicionado para especificações dos parâmetros dessa instance. Updated in 00:00

--- scratch.cpp
+++ scratch.cpp
@@ -18,6 +18,10 @@ public:
     }
 
 private: 
+    
+    VkInstance instance;
+    
+    
     void initWindow() { 
     
     glfwInit();
@@ -31,7 +35,7 @@ private:
     }    
 
     void initVulkan() {
-
+        createInstance();
     }
 
     void mainLoop() {
@@ -49,6 +53,27 @@ private:
     glfwTerminate();
 
     }
+    
+    void createInstance() {
+    
+         VkApplicationInfo appInfo{};
+         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+         appInfo.pApplicationName = "Hello Triangle";
+         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+         appInfo.pEngineName = "No Engine";
+         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+         appInfo.apiVersion = VK_API_VERSION_1_0;
+    
+         VkInstanceCreateInfo createInfo{};
+         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+         createInfo.pApplicationInfo = &appInfo;
+    
+    
+    
+    
+    }
+     
+    
 };
 
 int main() {
