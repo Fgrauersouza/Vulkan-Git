eda3009b11a768fcc8a6675d6bedc284145d9a3c
Fábio Souza <37919743+Fgrauersouza@users.noreply.github.com>
2024-08-01 00:01:09 -0300

    Alterações em instance
    Criação da instance e bloco de código INCOMPLETO, adicionado para especificações dos parâmetros dessa instance. Updated in 00:00

--- scratch.cpp
+++ scratch.cpp
@@ -67,10 +67,21 @@ private:
          VkInstanceCreateInfo createInfo{};
          createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
          createInfo.pApplicationInfo = &appInfo;
-    
-    
-    
-    
+         
+         uint32_t glfwExtensionCount = 0;
+         const char** glfwExtensions;
+
+         glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+         createInfo.enabledExtensionCount = glfwExtensionCount;
+         createInfo.ppEnabledExtensionNames = glfwExtensions;
+         
+         createInfo.enabledLayerCount = 0;
+
+         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create instance!");
+    }
+       
     }
      
     
